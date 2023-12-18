a58b696b6393ff971ecef4347c095327d3e3b6cc
van wsl2 <pvphat25@gmail.com>
2023-12-18 15:40:23 -0500

    set up connection pool to mysql 3306

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/e-commerce-backend
 
 go 1.21.5
+
+require github.com/go-sql-driver/mysql v1.7.1
--- main.go
+++ main.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type DBConfig struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Hostname string `json:"hostname"`
+	Dbname   string `json:"dbname"`
+	Net      string `json:"net"`
+}
+
+var DB *sql.DB
 
 func main() {
-	fmt.Println("Something")
+	// database file
+	jsonFile, err := os.Open("config/mysql_config.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// byte[]
+	byteValue, err := ioutil.ReadAll(jsonFile)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// parse json
+	var dbConfig DBConfig
+
+	err = json.Unmarshal([]byte(byteValue), &dbConfig)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// mysql config
+
+	cfgFormatted := mysql.Config{
+		User:   dbConfig.Username,
+		Passwd: dbConfig.Password,
+		Net:    dbConfig.Net,
+		Addr:   dbConfig.Hostname,
+		DBName: dbConfig.Dbname,
+	}
+
+	DB, err = sql.Open("mysql", cfgFormatted.FormatDSN())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := DB.Ping()
+
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	fmt.Println("Connected to MYSQL server successfully!")
+	defer jsonFile.Close()
 }
